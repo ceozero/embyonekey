7094582875e113fc24fbf2c680b94932e75bd7c2
ceozero <ceozero@me.com>
2020-10-23 20:07:10 +0800

    Update embyonekey.sh

--- embyonekey.sh
+++ embyonekey.sh
@@ -2,7 +2,7 @@
 cd /var/packages/EmbyServer/target/mono/bin
 curl https://raw.githubusercontent.com/ceozero/embyonekey/master/mb3admin.com.cert.pem >> /etc/ssl/certs/ca-certificates.crt
 ./cert-sync /etc/ssl/certs/ca-certificates.crt
-cd /usr/local/etc/nginx/conf.d/*-*-*-*
-wget https://raw.githubusercontent.com/ceozero/embyonekey/master/user.conf
-nginx -s reload
+#cd /usr/local/etc/nginx/conf.d/*-*-*-*
+#wget https://raw.githubusercontent.com/ceozero/embyonekey/master/user.conf
+#nginx -s reload
 
